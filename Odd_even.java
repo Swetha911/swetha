4f5b8a5170bad4f41f7bff3c57436c0024b163c2
Swetha911 <Swetha911@users.noreply.github.com>
2016-07-02 21:56:17 +0530

    Create Odd_even.java

---
+++ Odd_even.java
@@ -0,0 +1,20 @@
+
+package odd_even;
+
+public class Odd_even {
+
+   
+    public static void main(String[] args) {
+       
+        int n=11;
+        if(n%2==0) //to check whether the number is odd or even
+        {
+            System.out.println("even\t"+n);
+        }
+            else
+        {
+            System.out.println("odd\t"+n);
+        }
+        
+    }
+}
